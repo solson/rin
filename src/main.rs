f07465f053730316f037549a30dfdeb18740be04
Scott Olson <scott@solson.me>
2016-06-21 18:56:16 -0600

    Print help on no arguments in a better way.

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,22 @@
 extern crate clap;
 
-use clap::{Arg, App};
+use clap::{Arg, App, AppSettings};
 use std::ffi::OsStr;
 use std::fs::File;
 use std::io;
 
 fn main() {
-    let app = App::new(env!("CARGO_PKG_NAME"))
+    let matches = App::new(env!("CARGO_PKG_NAME"))
         .version(env!("CARGO_PKG_VERSION"))
         .author("Scott Olson <scott@solson.me>")
         .about(env!("CARGO_PKG_DESCRIPTION"))
+        .setting(AppSettings::ArgRequiredElseHelp)
         .arg(Arg::with_name("FILE")
              .multiple(true)
              .required(true)
-             .help("The file(s) to display"));
+             .help("The file(s) to display"))
+        .get_matches();
 
-    if std::env::args().len() == 1 {
-        app.print_help().unwrap();
-        println!("");
-        return;
-    }
-
-    let matches = app.get_matches();
     let files: Vec<&OsStr> = matches.values_of_os("FILE").unwrap().collect();
 
     let mut first = true;
